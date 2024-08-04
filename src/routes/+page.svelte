8621a0307a1a37d9ff4376094d6109ab0ae63da6
Schischkin <hamza@schischkin.info>
2024-08-04 21:49:10 +0200

    feat: add basic information about myself

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Hallo Welt</h1>
-
+<div
+	class="flex min-h-screen w-auto flex-col items-center gap-6 rounded-lg bg-white py-8 shadow-md"
+>
+	<img
+		class="block h-32 w-32 rounded-full bg-sky-200 md:h-64 md:w-64"
+		src="/me.png"
+		alt="Daniel Schischkin Memoji"
+	/>
+	<div>
+		<h1 class="text-center text-2xl font-semibold tracking-tight text-gray-900 md:text-4xl">
+			Daniel Schischkin
+		</h1>
+		<p class="text-nowrap text-center font-light text-gray-600 md:text-lg">
+			Senior Software Developer @ <a href="https://btc-ag.com">BTC AG</a>
+		</p>
+	</div>
+	<div>
+		<a
+			class="pointer rounded border border-gray-300 px-4 py-2 text-xs text-gray-900 transition-all duration-200 ease-in-out hover:shadow-md"
+			href="https://www.linkedin.com/in/daniel-schischkin-67336a144/"
+			target="_blank">Connect with me!</a
+		>
+	</div>
+</div>
